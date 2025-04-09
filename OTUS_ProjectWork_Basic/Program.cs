ac86787ee1b5f2e2ec947c817123b832a413bfd3
Ilya Shadrin <90275369+dekey111@users.noreply.github.com>
2025-04-09 23:45:50 +0300

    dev

--- OTUS_ProjectWork_Basic/Program.cs
+++ OTUS_ProjectWork_Basic/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using OTUS_ProjectWork_Basic.DataBase;
+using OTUS_ProjectWork_Basic.Repositories;
+
 Console.WriteLine("Hello, World!");
+
+var context = new CloudReaderContext();
+
+var AuthorRepo = new Repository<Author>(context);
+var allAuthors = AuthorRepo.GetAll();
+Console.WriteLine("Все авторы: " + string.Join("", allAuthors.Select(x => $"{x.Id} | {x.Fullname}\n")));
